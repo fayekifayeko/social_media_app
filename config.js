1007225a7bba563c613f8688994a774166f30eed
Faeq <fayekmir@gmail.com>
2022-03-11 21:08:18 +0100

    fixing

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    url: 'mongodb+srv://fslim:45SOwnK2l470QlsG@cluster0.54krr.mongodb.net/social-media?retryWrites=true&w=majority',
-    SECRET_KEY: 'TMGF%(#!"KFKKFDKF)DKDCDCKD'
-} // shouldn't pushed to git, you can use .env files instead
