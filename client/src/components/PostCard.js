3e4e7179d2bc1971f64cfa07f6c7927b0b08f54b
Faeq <fayekmir@gmail.com>
2022-03-14 20:41:49 +0100

    register user

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import moment from 'moment';
-import { Card, Image } from 'semantic-ui-react';
+import { Card, Image, Button, Icon, Label } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 export default function PostCard({post: { body, createdAt, id, userName, likeCount, commentCount, likes}}) {
+
+  function likePost() {
+    console.log('like');
+  }
+
+  function commentPost() {
+    console.log('comment');
+  }
 
     return(
         <Card fluid>
@@ -20,8 +28,25 @@ export default function PostCard({post: { body, createdAt, id, userName, likeCou
         <Card.Description>{body}</Card.Description>
         </Card.Content>
           <Card.Content extra>
-           <h1>buttons here</h1>
-          </Card.Content>
+          <div>
+    <Button basic as='div' labelPosition='right' onClick={likePost}>
+      <Button basic color='teal'>
+        <Icon name='heart' />
+      </Button>
+      <Label as='a' basic color='teal' pointing='left'>
+        {likeCount}
+      </Label>
+    </Button>
+    <Button as='div' labelPosition='right' onClick={commentPost}>
+      <Button basic color='blue'>
+        <Icon name='comments' />
+      </Button>
+      <Label as='a' basic color='blue' pointing='left'>
+        {commentCount}
+      </Label>
+    </Button>
+  </div>
+    </Card.Content>
         </Card>
     )
 }
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Form } from 'semantic-ui-react'
+import { useMutation } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
 function Register() {
+    const [values, setValues] = useState({
+        userName: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    });
+
+    const[addUser, {loading}]  = useMutation(REGISTER_USER_MUTATION,
+        {
+            update(proxy, result) {
+                console.log(result);
+            },
+            variables: values
+        }
+        )
+
+    const onChange = (e) => setValues({...values, [e.target.name]: e.target.value});
+const onSubmit = () => {
+
+}
+
+const REGISTER_USER_MUTATION = gql`
+mutation register(
+    $userName: string!, 
+    $email: String!, 
+    $password: String!, 
+    $confirmPassword: String
+    ){
+    register(
+        registerInput: {userName: $userName, email: $email, password: $password, confirmPassword: $confirmPassword}
+    ){
+        id
+      email
+      username
+      createdAt
+      token
+    }
+}
+`
+
     return(
         <div>
-            <h1>Register</h1>
+            <Form onSubmit={handleSubmit} noValidate>
+                <h1>Register</h1>
+            <Form.Input label='User name' placeholder='Username' name='userName'  value={values.userName} onChange={onChange}/>
+
+            <Form.Input label='Email' placeholder='Email...' name='email'  value={values.email} onChange={onChange}/>
+
+            <Form.Input label='Password' placeholder='Password...' name='password'  value={values.password} onChange={onChange}/>
+            <Form.Input label='Confirm password' placeholder='Confirm password' name='confirmPassword'  value={values.confirmPassword} onChange={onChange}/>
+
+    <Button type='submit' primary>Register</Button>
+  </Form>
         </div>
     )
 }
